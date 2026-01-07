318478233278c359c584c868378ed148db5a07e8
Robert <robertpopa015@gmail.com>
2026-01-07 14:17:13 +0200

    Mici modificari finale

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,9 @@ module smarthome {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires javafx.media; // Uneori necesar pentru AWT/Swing utilities
+    requires javafx.media;
 
-    // Deschidem pachetul tau catre JavaFX ca sa poata incarca fereastra
+    // Deschidem pachetul catre JavaFX ca sa poata incarca fereastra
     opens smarthome to javafx.fxml;
 
     // Exportam pachetul tau ca sa poata fi rulat
--- src/main/java/smarthome/Launcher.java
+++ src/main/java/smarthome/Launcher.java
@@ -2,7 +2,6 @@ package smarthome;
 
 public class Launcher {
     public static void main(String[] args) {
-        // Apeleaza metoda main din clasa ta principala JavaFX
         MainApp.main(args);
     }
 }
